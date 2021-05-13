78f37619a2b65cc74ff47b97152e375654e02817
mmockelyn <mmockelyn@gmail.com>
2021-05-13 21:04:28 +0200

    [Feat] - Système de gestion de packages  -> Initialisation
    [Bugfix] - Login Steam not provide if nickname hidden

--- app/Http/Controllers/Account/PackageController.php
+++ app/Http/Controllers/Account/PackageController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Account;
 
 use App\Http\Controllers\Controller;
+use App\Packages\SteamApi\Steam;
 use App\Repository\Download\DownloadRepository;
 use Illuminate\Http\Request;
+use Syntax\SteamApi\Facades\SteamApi;
 
 class PackageController extends Controller
 {
@@ -12,14 +14,20 @@ class PackageController extends Controller
      * @var DownloadRepository
      */
     private DownloadRepository $downloadRepository;
+    /**
+     * @var Steam
+     */
+    private Steam $steam;
 
     /**
      * PackageController constructor.
      * @param DownloadRepository $downloadRepository
+     * @param Steam $steam
      */
-    public function __construct(DownloadRepository $downloadRepository)
+    public function __construct(DownloadRepository $downloadRepository, Steam $steam)
     {
         $this->downloadRepository = $downloadRepository;
+        $this->steam = $steam;
     }
 
 
