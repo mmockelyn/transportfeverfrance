5cb5ee047eedefe9e5b6ddc3707f97094d524a23
mmockelyn <mmockelyn@gmail.com>
2021-06-30 01:23:19 +0200

    [Bugfix] - Correct Route HoneySpamDetector
    close #62, #63

--- routes/front/front.php
+++ routes/front/front.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 include('auth.php');
 Route::name('home')->get('/', [BlogController::class, 'index']);
-Route::middleware(['honey'])->resource('contacts', ContactController::class, ['only' => ['create', 'store']]);
+Route::resource('contacts', ContactController::class, ['only' => ['create', 'store']]);
 Route::name('page')->get('page/{page:slug}', PageController::class);
 Route::get('/unlock/tffrance', [\App\Http\Controllers\HomeController::class, 'tffrance']);
 include('blog.php');
