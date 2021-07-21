9e8b154dcb8e44d0b9f27e0aac84010beef25610
mmockelyn <mmockelyn@gmail.com>
2021-07-21 11:28:30 +0200

    [Feat] - Ajout d'un widget discord sur les pages

--- resources/views/front/blog/category.blade.php
+++ resources/views/front/blog/category.blade.php
@@ -92,6 +92,7 @@
                             </div>
                             @endforeach
                         </div>
+                        <iframe src="https://discord.com/widget?id=709827298441822257&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
                     </div>
                 </div>
             </div>
--- resources/views/front/blog/show.blade.php
+++ resources/views/front/blog/show.blade.php
@@ -120,6 +120,9 @@
                     </div>
                 </div>
             </div>
+            <div class="col-3">
+                <iframe src="https://discord.com/widget?id=709827298441822257&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            </div>
         </div>
     </div>
 @endsection
