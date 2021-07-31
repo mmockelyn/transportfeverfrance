6090c3ef5a36684f31081fdd9cf3db587cbe4d20
mmockelyn <mmockelyn@gmail.com>
2021-08-01 01:03:33 +0200

    [Feat] - Initialisation de la gestion des articles du blog

--- app/Http/Controllers/Back/Blog/BlogController.php
+++ app/Http/Controllers/Back/Blog/BlogController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Back\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog\Blog;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
     public function dashboard()
     {
-        return view('back.blog.dashboard');
+        return view('back.blog.dashboard', [
+            "blogs" => Blog::all()
+        ]);
     }
 }
